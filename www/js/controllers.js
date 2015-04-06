543bd2e6ec4ac04e90c257ba9e32f07fbdc3ecd1
vkondhekar <vishwanath.kondhekar@sjrb.ca>
2015-04-06 19:44:11 +0530

    Completed full UI

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,28 @@
-angular.module('starter.controllers', [])
+angular
+		.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+		.controller(
+				'FDCnrl',
+				function($scope) {
+					$scope.finalAmt = ($scope.txtDeposit * $scope.txtROI * $scope.txtPeriod) / 100;
+				})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+		.controller('DashCtrl', function($scope) {
+		})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+		.controller('ChatsCtrl', function($scope, Chats) {
+			$scope.chats = Chats.all();
+			$scope.remove = function(chat) {
+				Chats.remove(chat);
+			}
+		})
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+		.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+			$scope.chat = Chats.get($stateParams.chatId);
+		})
+
+		.controller('AccountCtrl', function($scope) {
+			$scope.settings = {
+				enableFriends : true
+			};
+		});
